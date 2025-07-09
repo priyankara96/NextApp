52d77d89e5b1391fac83010e18eb8d6d5b5bc80d
Aseem3cs <aseem@3cs.solutions>
2025-07-09 12:15:37 +0530

    Create next app

---
+++ my-app/src/app/test/page.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <h1>Page 02</h1>
+        <Link href={'/'}>Go to page 01</Link>
+
+      </main>
+    </div >
+  );
+}
